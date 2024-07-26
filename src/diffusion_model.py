14a9773619d2fb43da7c9efae9b87dd096a1f60f
ben <ben@Ns-MacBook-Pro.local>
2024-07-26 11:14:36 -0700

    fix implemenation for superresolution

--- src/diffusion_model.py
+++ src/diffusion_model.py
@@ -9,6 +9,73 @@ import torch.nn as nn
 from typing import Dict, Tuple
 
 
+class GaussianDiffusionModelSR(nn.Module):
+    def __init__(
+        self,
+        eps_model: nn.Module,
+        betas: Tuple[float, float],
+        n_T: int,
+        criterion: nn.Module = nn.MSELoss(),
+    ) -> None:
+        super(GaussianDiffusionModelSR, self).__init__()
+        
+        
+        self.eps_model = eps_model
+
+        # register_buffer allows us to freely access these tensors by name. It helps device placement.
+        for k, v in ddpm_schedules(betas[0], betas[1], n_T).items():
+            self.register_buffer(k, v)
+
+        self.n_T = n_T
+        self.criterion = criterion
+
+    def forward(self,  snapshots: torch.Tensor, conditioning_snapshots: torch.Tensor) -> torch.Tensor:
+        """
+        Forward diffusion: tries to guess the epsilon value from snapshot_t using the eps_model.
+        See Alg 1 in https://arxiv.org/pdf/2006.11239
+        """
+
+        conditioning_snapshots_interpolated = nn.functional.interpolate(conditioning_snapshots, 
+                                                                        size=[snapshots.shape[2], snapshots.shape[3]], 
+                                                                        mode='bilinear')
+
+        residual_snapshots = snapshots - conditioning_snapshots_interpolated
+        
+
+        _ts = torch.randint(1, self.n_T + 1, (snapshots.shape[0],)).to(snapshots.device) # t ~ Uniform(0, n_T)
+        eps = torch.randn_like(snapshots)  # eps ~ N(0, 1)
+
+        # This is the x_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
+        snapshots_t = (self.sqrtab[_ts, None, None, None] * residual_snapshots + self.sqrtmab[_ts, None, None, None] * eps)  
+
+        
+        # We should predict the "error term" from this x_t. Loss is what we return.
+        eps_predicrted = self.eps_model(snapshots_t, _ts / self.n_T, lowres_snapshot=conditioning_snapshots_interpolated)
+        return self.criterion(eps, eps_predicrted)
+
+
+
+    def sample(self, n_sample: int, size, conditioning_snapshots: torch.Tensor, device='cuda') -> torch.Tensor:
+        """
+        Let's sample
+        See Alg 2 in https://arxiv.org/pdf/2006.11239
+        """
+        snapshots_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1)
+        
+
+        conditioning_snapshots_interpolated = nn.functional.interpolate(conditioning_snapshots[0:snapshots_i.shape[0]], 
+                                                                       size=[snapshots_i.shape[2], snapshots_i.shape[3]], 
+                                                                       mode='bilinear')
+
+        for i in range(self.n_T, 0, -1):
+            z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
+            eps = self.eps_model(snapshots_i, torch.tensor(i / self.n_T).to(device).repeat(n_sample), lowres_snapshot=conditioning_snapshots_interpolated)
+            snapshots_i = (self.oneover_sqrta[i] * (snapshots_i - eps * self.mab_over_sqrtmab[i]) + self.sqrt_beta_t[i] * z)
+
+        return snapshots_i + conditioning_snapshots_interpolated
+    
+    
+    
 class GaussianDiffusionModel(nn.Module):
     def __init__(
         self,
@@ -29,7 +96,7 @@ class GaussianDiffusionModel(nn.Module):
         self.n_T = n_T
         self.criterion = criterion
 
-    def forward(self,  snapshots: torch.Tensor, conditioning_snapshots = None) -> torch.Tensor:
+    def forward(self,  snapshots: torch.Tensor) -> torch.Tensor:
         """
         Forward diffusion: tries to guess the epsilon value from snapshot_t using the eps_model.
         See Alg 1 in https://arxiv.org/pdf/2006.11239
@@ -39,41 +106,27 @@ class GaussianDiffusionModel(nn.Module):
         eps = torch.randn_like(snapshots)  # eps ~ N(0, 1)
 
         # This is the x_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
-        snapshots_t = (self.sqrtab[_ts, None, None, None] * snapshots + self.sqrtmab[_ts, None, None, None] * eps)  
-
-        if conditioning_snapshots is not None:
-            conditioning_snapshots_interpolated = nn.functional.interpolate(conditioning_snapshots, 
-                                                                        size=[snapshots.shape[2], snapshots.shape[3]], 
-                                                                        mode='bilinear')
-        else:
-            conditioning_snapshots_interpolated = None
+        snapshots_t = (self.sqrtab[_ts, None, None, None] * snapshots + self.sqrtmab[_ts, None, None, None] * eps)          
 
         # We should predict the "error term" from this x_t. Loss is what we return.
-        eps_predicrted = self.eps_model(snapshots_t, _ts / self.n_T, lowres_snapshot=conditioning_snapshots_interpolated)
+        eps_predicrted = self.eps_model(snapshots_t, _ts / self.n_T)
         return self.criterion(eps, eps_predicrted)
 
 
 
-    def sample(self, n_sample: int, size, conditioning_snapshots=None, device='cuda') -> torch.Tensor:
+    def sample(self, n_sample: int, size, device='cuda') -> torch.Tensor:
         """
         Let's sample
         See Alg 2 in https://arxiv.org/pdf/2006.11239
         """
         snapshots_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1)
         
-        if conditioning_snapshots is not None:
-            conditioning_snapshots_interpolated = nn.functional.interpolate(conditioning_snapshots[0:snapshots_i.shape[0]], 
-                                                                       size=[snapshots_i.shape[2], snapshots_i.shape[3]], 
-                                                                       mode='bilinear')
-
         for i in range(self.n_T, 0, -1):
             z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
-            eps = self.eps_model(snapshots_i, torch.tensor(i / self.n_T).to(device).repeat(n_sample), lowres_snapshot=conditioning_snapshots_interpolated)
+            eps = self.eps_model(snapshots_i, torch.tensor(i / self.n_T).to(device).repeat(n_sample))
             snapshots_i = (self.oneover_sqrta[i] * (snapshots_i - eps * self.mab_over_sqrtmab[i]) + self.sqrt_beta_t[i] * z)
 
-        return snapshots_i
-    
-    
+        return snapshots_i    
     
 
 def ddpm_schedules(beta1: float, beta2: float, T: int) -> Dict[str, torch.Tensor]:
